4d38b611431dc939301bfc4f67eeb98701aace16
fikerte <tofikertesfaye@gmail.com>
2024-10-02 20:07:10 +0300

    Setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: { 
+      colors: {
+        'colorgreen': 'hsl(75, 94%, 57%)',
+        'colorgray500': 'hsl(0, 0%, 80%)',
+        'colorgray700': 'hsl(0, 0%, 20%)',
+        'colorgray800': 'hsl(0, 0%, 12%)',
+        'colorgray900': 'hsl(0, 0%, 8%)',
+        'colorwhite': '#fff',
+      },
+      fontFamily: {
+        Inter: ["Inter", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+
